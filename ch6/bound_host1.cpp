9510d606dcea09a425d92b19afafc7aa4a966980
zone-1614 <2493920954@qq.com>
2024-05-27 19:23:39 +0800

    1

--- ch6/bound_host1.cpp
+++ ch6/bound_host1.cpp
@@ -13,7 +13,8 @@
 void error_handling(const char *message);
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
+
+    if (argc == 2) {
         printf("Usage: %s <port>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
@@ -22,24 +23,24 @@ int main(int argc, char *argv[]) {
     if (sock == -1) 
         error_handling("socket() error");
     
-    sockaddr_in my_adr, your_adr;
+    sockaddr_in my_adr;
     memset(&my_adr, 0, sizeof(my_adr));
     my_adr.sin_family = AF_INET;
     my_adr.sin_addr.s_addr = htonl(INADDR_ANY);
     my_adr.sin_port = htons(atoi(argv[1]));
 
-    if (bind(sock, reinterpret_cast<sockaddr*>(&my_adr), sizeof(my_adr)) == -1)
+    if (bind(sock, reinterpret_cast<sockaddr*>(&my_adr), sizeof(my_adr)) == -1) 
         error_handling("bind() error");
 
+    sockaddr_in your_adr;
+    char message[BUF_SIZE];
     socklen_t adr_sz;
     int str_len;
-    char message[BUF_SIZE];
     for (int i = 0; i < 3; i++) {
         sleep(5);
         adr_sz = sizeof(your_adr);
         str_len = recvfrom(sock, message, BUF_SIZE, 0, 
             reinterpret_cast<sockaddr*>(&your_adr), &adr_sz);
-        
         printf("Message: %d: %s\n", i + 1, message);
     }
     close(sock);
