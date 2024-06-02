81cb74efb6afee630c565a23653bdc6bc38c6ead
zone-1614 <2493920954@qq.com>
2024-06-02 23:13:44 +0800

    ch8

--- ch8/gethostbyaddr.cpp
+++ ch8/gethostbyaddr.cpp
@@ -1,23 +1,27 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
+#include <string>
+#include <string_view>
 
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <netdb.h>
 
-void error_handling(const char* message) {
-    perror(message);
+#include <fmt/format.h>
+
+void error_handling(std::string_view msg) {
+    perror(msg.data());
     exit(EXIT_FAILURE);
 }
 
 int main(int argc, char* argv[]) {
     if (argc != 2) {
-        printf("Usage: %s <addr>\n", argv[0]);
+        fmt::println("Usage: {} <addr>", argv[0]);
         exit(EXIT_FAILURE);
     }
 
-    sockaddr_in addr;
+    struct sockaddr_in addr;
     memset(&addr, 0, sizeof(addr));
     addr.sin_addr.s_addr = inet_addr(argv[1]);
     hostent* host = gethostbyaddr(reinterpret_cast<void*>(&addr.sin_addr), 4, AF_INET);
@@ -25,12 +29,12 @@ int main(int argc, char* argv[]) {
         error_handling("gethostbyaddr error");
     }
     
-    printf("Official name: %s\n", host->h_name);
+    fmt::println("Official name: {}", host->h_name);
     for (int i = 0; host->h_aliases[i] != nullptr; i++) {
-        printf("Aliases %d: %s\n", i + 1, host->h_aliases[i]);
+        fmt::println("Aliases {}: {}", i + 1, host->h_aliases[i]);
     }
-    printf("Address type: %s\n", (host->h_addrtype == AF_INET) ? "AF_INET" : "AF_INET 6");
+    fmt::println("Address type: {}", (host->h_addrtype == AF_INET) ? "AF_INET" : "AF_INET 6");
     for (int i = 0; host->h_addr_list[i] != nullptr; i++) {
-        printf("IP addr %d: %s\n", i + 1, inet_ntoa(*(reinterpret_cast<in_addr*>(host->h_addr_list[i]))));
+        fmt::println("IP addr {}: {}", i + 1, inet_ntoa(*(reinterpret_cast<in_addr*>(host->h_addr_list[i]))));
     }
 }
