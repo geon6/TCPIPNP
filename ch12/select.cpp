38f96208edf93f8b09e747a5130ba6c3852eb612
zone-1614 <2493920954@qq.com>
2024-06-03 12:22:13 +0800

    ch11 ch12

--- ch12/select.cpp
+++ ch12/select.cpp
@@ -5,7 +5,9 @@
 #include <sys/time.h>
 #include <sys/select.h>
 
-#define BUF_SIZE 30
+#include <fmt/format.h>
+
+constexpr int BUF_SIZE = 30;
 
 int main(int argc, char* argv[]) {
     fd_set reads, temps;
@@ -26,15 +28,15 @@ int main(int argc, char* argv[]) {
         timeout.tv_usec = 0;
         result = select(1, &temps, 0, 0, &timeout);
         if (result == -1) {
-            std::cout << "select() error" << std::endl;
+            fmt::println("select() error");
             break;
         } else if (result == 0) {
-            std::cout << "Time-out!" << std::endl;
+            fmt::println("Time-out!");
         } else {
             if (FD_ISSET(0, &temps)) {
                 str_len = read(0, buf, BUF_SIZE);
                 buf[str_len] = 0;
-                std::cout << "message from console: " << buf << std::endl;
+                fmt::println("message from console: {}", buf);
             }
         }
     }
